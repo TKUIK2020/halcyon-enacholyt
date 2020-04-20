e67246d4bfcf9fa63275393d38acf0f78db8a8c9
enacholyt <62543651+enacholyt@users.noreply.github.com>
2020-04-20 11:39:13 +0300

    Create k.js

---
+++ 10/k.js
@@ -0,0 +1,12 @@
+
+require("http").createServer((req, res) => {
+    if (req.url === "/") {
+      res.statusCode = 200;
+  
+      res.setHeader("Content-Type", "image/jpeg");
+  
+      require("fs").readFile("kartinka.jpg", (err, image) => {
+        res.end(image);
+      });
+    }
+  }).listen(3000, () => console.log('server ON'));
